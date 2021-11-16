c45365363ccb8701b8e667f9a51c83483803e263
imenesg <imenesgabriel@gmail.com>
2021-11-16 16:36:04 -0300

    Add some emojis

--- js/alert.js
+++ js/alert.js
@@ -4,7 +4,7 @@ var botao = document.querySelectorAll(".alerta")
 for (let index = 0; index < botao.length; index++) {
     botao[index].addEventListener("click",()=>{
     console.log("foi");
-        alert("obrigado por visitar o projeto :)")
+        alert(" 🍅Obrigado por visitar o projeto :)🥑")
     });
     
 }
